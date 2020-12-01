2aede697df87fa705fee6ea3fa7366b7d3b8e20a
zzlweb <2736815971@qq.comgit config --global user.email .2736815971@qq.com>
2020-12-01 21:40:03 +0800

    添加提示

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view v-if="flag" />
+
+    <div v-else class="flex-col resize-container">
+      <img src="./assets/svg/warning.svg" alt="">
+      <div class="alert-text">请在PC端打开</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      screenWidth: document.documentElement.clientWidth,
+      flag: true
+    }
+  },
+  watch: {
+    screenWidth(val) {
+      console.log(val)
+      val < 1100 ? this.flag = false : this.flag = true
+    }
+  },
+  mounted() {
+    if (this.screenWidth < 1100) {
+      this.flag = false
+    }
+    window.addEventListener('resize', () => {
+      this.screenWidth = document.body.offsetWidth
+    })
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.resize-container{
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  .alert-text{
+    height: 40px;
+    line-height: 40px;
+  }
+}
+</style>
